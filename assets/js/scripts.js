2d710ee396b1ee57efa478a4c3097960efe15a31
diegoh22 <diegohofficial@gmail.com>
2021-09-09 16:04:38 +0000

    done al Js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
-function getComputerChoice(){
-    return Math.floor(Math.ramdom() * 5 + 1);
+function getComputerChoice() {
+    return Math.floor((Math.random() * 5) + 1);
 }
 
 const ROCK = 'ROCK';
@@ -25,70 +25,61 @@ const OPTIONS2EMOJI = {
 let userChoiceID = '';
 
 window.onload = () => {
-    const output = document.querySelector('#output');
-    
     const userOptions = document.querySelectorAll('.user-option');
 
     userOptions.forEach(el => el.addEventListener('click', event => {
         userChoiceID = event.target.id;
     }));
-    
+
     const playButton = document.querySelector('#play');
 
-    playButton.addEventListener('clik',() => {
+    playButton.addEventListener('click', () => {
         const computerChoiceID = getComputerChoice();
-        const computerChoice =  OPTIONS[computerChoiceID];
+        const computerChoice = OPTIONS[computerChoiceID];
         const computerChoiceElement = document.querySelector('#computer-choice');
+        const output = document.querySelector('#output');
         const userChoice = OPTIONS[userChoiceID];
 
-        let result ='';
-        
+        let result = '';
+
         computerChoiceElement.innerHTML = OPTIONS2EMOJI[computerChoiceID];
 
         switch (`${computerChoice}-${userChoice}`) {
-             case `${ROCK}-${ROCK}`:
-             case `${PAPER}-${PAPER}`:
-             case `${SCISSORS}-${SCISSORS}`:
-             case `${LIZARD}-${LIZARD}`:
-             case `${SPOCK}-${SPOCK}`:
-
-                 result = "TIE.👻"
-                 break;
-
+            case `${ROCK}-${ROCK}`:
+            case `${SCISSORS}-${SCISSORS}`:
+            case `${PAPER}-${PAPER}`:
+            case `${SPOCK}-${SPOCK}`:
+            case `${LIZARD}-${LIZARD}`:
+                result = 'TIE 👻'
+                break;
             case `${ROCK}-${SCISSORS}`:
             case `${ROCK}-${LIZARD}`:
             case `${SCISSORS}-${PAPER}`:
             case `${SCISSORS}-${LIZARD}`:
             case `${PAPER}-${ROCK}`:
             case `${PAPER}-${SPOCK}`:
-            case `${LIZARD}-${PAPER}`:
-            case `${LIZARD}-${SPOCK}`:
             case `${SPOCK}-${ROCK}`:
             case `${SPOCK}-${SCISSORS}`:
-
-                result = 'Computer Win.💔'
+            case `${LIZARD}-${PAPER}`:
+            case `${LIZARD}-${SPOCK}`:
+                result = 'COMPUTER WIN 😔💔 🤖🥇'
                 break;
-
             case `${ROCK}-${PAPER}`:
             case `${ROCK}-${SPOCK}`:
             case `${SCISSORS}-${ROCK}`:
             case `${SCISSORS}-${SPOCK}`:
             case `${PAPER}-${SCISSORS}`:
             case `${PAPER}-${LIZARD}`:
-            case `${LIZARD}-${ROCK}`:
-            case `${LIZARD}-${SCISSORS}`:
             case `${SPOCK}-${PAPER}`:
             case `${SPOCK}-${LIZARD}`:
-
-                result = 'You Win.✌.⭐'
+            case `${LIZARD}-${ROCK}`:
+            case `${LIZARD}-${SCISSORS}`:
+                result = 'YOU WIN ☺️🥇 🤖💔'
                 break;
-
             default:
-            result = 'Someting Wrong. try again'
-
+                result = 'SOMETHING WRONG. TRY AGAIN. 💀'
         }
 
         output.innerHTML = result;
-
-    });
-};
+    })
+}
